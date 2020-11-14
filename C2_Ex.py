91dc21bc7e37f138e3e249f7c68823195bfd68d3
JoeEdoh <Josephedoh77@gmail.com>
2020-11-14 04:21:43 +0100

    this code receives the name input from a user and outputs the name with a greeting

---
+++ C2_Ex.py
@@ -0,0 +1,18 @@
+# Question 1
+import sys
+name = sys.argv[1]
+print("Hello",name)
+
+
+'''
+# Question 2
+hours = input("Enter Hours: ")
+rate = input("Enter Rate: ")
+pay = int(hours) * float(rate)
+print(pay)
+
+# Question 3 Temperature Converter
+T_C = input('Enter Temperature in Celsius: ')
+T_F = (int(T_C) + 32)*5/9
+print(T_F)
+'''
