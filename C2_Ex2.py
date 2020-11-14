fd6108938d1a67e8faf9d3895f8dd33727bbbb14
JoeEdoh <Josephedoh77@gmail.com>
2020-11-14 04:23:54 +0100

    The following codes are the answers for the  remaining exercises in chapter 1. C2_Ex2.py  converts hours and rates into work rates C3_Ex3.py  converts temperature in celsius to fahrenheit

---
+++ C2_Ex2.py
@@ -0,0 +1,5 @@
+# Question 2
+hours = input("Enter Hours: ")
+rate = input("Enter Rate: ")
+pay = int(hours) * float(rate)
+print(pay)
---
+++ C2_Ex3.py
@@ -0,0 +1,5 @@
+# Question 3 Temperature Converter
+T_C = input('Enter Temperature in Celsius: ')
+T_F = (int(T_C) + 32)*5/9
+print(T_F)
+
